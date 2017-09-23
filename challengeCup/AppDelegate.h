4a4a23adf5670965a062a14f3463abd0c33d8914
lzLiu <1015320177@qq.com>
2017-09-23 15:42:06 +0800

    Initial Commit

---
+++ challengeCup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  challengeCup
+//
+//  Created by 柳麟喆 on 2017/9/23.
+//  Copyright © 2017年 lzLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
